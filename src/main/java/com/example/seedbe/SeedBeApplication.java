342812ef69e26d6a7d5631b1cc82b3dee28a56a8
yangjunsig <wnstlr1379@naver.com>
2026-03-03 21:08:18 +0900

    first commit

---
+++ src/main/java/com/example/seedbe/SeedBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.seedbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeedBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seedbe/SeedBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seedbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
