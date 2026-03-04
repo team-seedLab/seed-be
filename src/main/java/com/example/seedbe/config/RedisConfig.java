39c92467c66b96b91595c585557a040e27591a48
yangjunsig <wnstlr1379@naver.com>
2026-03-05 02:21:09 +0900

    feature: 레디스 기본 연결 설정 코드 추가

---
+++ src/main/java/com/example/seedbe/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.example.seedbe.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    }
+
+    // 리프레시 토큰 저장/조회를 위한 템플릿
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
+        return stringRedisTemplate;
+    }
+}
