8bbdaef60fff5e1510d14e271f4bb43de92acb24
yangjunsig <wnstlr1379@naver.com>
2026-03-08 14:28:57 +0900

    feature: jwt 설정 객체화

---
+++ src/main/java/com/example/seedbe/global/security/jwt/JwtProperties.java
@@ -0,0 +1,11 @@
+package com.example.seedbe.global.security.jwt;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "jwt")
+public record JwtProperties(
+        String secret,
+        long accessTokenExpiration,
+        long refreshTokenExpiration
+) {
+}
