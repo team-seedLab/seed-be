1bca84e3566b4701abe96b953437e91eccf84840
yangjunsig <wnstlr1379@naver.com>
2026-03-09 21:31:32 +0900

    refactor: 쿠키유틸 서브도메인 설정 명시화 및 코드 리펙토링

--- src/main/java/com/example/seedbe/global/util/CookieUtil.java
+++ src/main/java/com/example/seedbe/global/util/CookieUtil.java
@@ -3,29 +3,47 @@ package com.example.seedbe.global.util;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
 
 public class CookieUtil {
 
     // 쿠키 생성 (굽기)
     public static void addCookie(HttpServletResponse response, String name, String value, int maxAge, boolean isSecure) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setPath("/");
-        cookie.setHttpOnly(true); // XSS 공격 방어
-        cookie.setMaxAge(maxAge);
-        cookie.setSecure(isSecure);
-        response.addCookie(cookie);
+
+        String domain = isSecure ? "seedlab.cloud" : null;
+
+        ResponseCookie cookie = ResponseCookie.from(name, value)
+                .path("/")
+                .domain(domain)
+                .httpOnly(true)
+                .maxAge(maxAge)
+                .secure(isSecure)
+                .sameSite("Lax")
+                .build();
+
+        response.addHeader("Set-Cookie", cookie.toString());
     }
 
     // 쿠키 삭제 (로그아웃 시 사용)
-    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
+    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name, boolean isSecure) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals(name)) {
-                    cookie.setValue("");
-                    cookie.setPath("/");
-                    cookie.setMaxAge(0); // 수명을 0으로 만들어서 브라우저가 삭제하게 만듦
-                    response.addCookie(cookie);
+
+                    String domain = isSecure ? "seedlab.cloud" : null;
+
+                    ResponseCookie deleteCookie = ResponseCookie.from(name, "")
+                            .path("/")
+                            .domain(domain)
+                            .httpOnly(true)
+                            .maxAge(0)
+                            .secure(isSecure)
+                            .sameSite("Lax")
+                            .build();
+
+                    response.addHeader("Set-Cookie", deleteCookie.toString());
+                    break;
                 }
             }
         }
