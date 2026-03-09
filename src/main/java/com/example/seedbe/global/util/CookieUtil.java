8e9b818217fa50e3bf0a3a406afab1c9be59d19f
yangjunsig <wnstlr1379@naver.com>
2026-03-09 21:31:47 +0900

    refactor: 쿠키유틸 서브도메인 설정 명시화 및 코드 리펙토링

--- src/main/java/com/example/seedbe/global/util/CookieUtil.java
+++ src/main/java/com/example/seedbe/global/util/CookieUtil.java
@@ -7,12 +7,13 @@ import org.springframework.http.ResponseCookie;
 
 public class CookieUtil {
 
-    // 쿠키 생성 (굽기)
-    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge, boolean isSecure) {
+    private static final String PROD_DOMAIN = "seedlab.cloud";
 
-        String domain = isSecure ? "seedlab.cloud" : null;
+    private static ResponseCookie generateCookie(String name, String value, int maxAge, boolean isSecure) {
+        // 확장성을 위한 서브도메인 공유 정책
+        String domain = isSecure ? PROD_DOMAIN : null;
 
-        ResponseCookie cookie = ResponseCookie.from(name, value)
+        return ResponseCookie.from(name, value)
                 .path("/")
                 .domain(domain)
                 .httpOnly(true)
@@ -20,28 +21,20 @@ public class CookieUtil {
                 .secure(isSecure)
                 .sameSite("Lax")
                 .build();
+    }
 
+    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge, boolean isSecure) {
+        ResponseCookie cookie = generateCookie(name, value, maxAge, isSecure);
         response.addHeader("Set-Cookie", cookie.toString());
     }
 
-    // 쿠키 삭제 (로그아웃 시 사용)
     public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name, boolean isSecure) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals(name)) {
-
-                    String domain = isSecure ? "seedlab.cloud" : null;
-
-                    ResponseCookie deleteCookie = ResponseCookie.from(name, "")
-                            .path("/")
-                            .domain(domain)
-                            .httpOnly(true)
-                            .maxAge(0)
-                            .secure(isSecure)
-                            .sameSite("Lax")
-                            .build();
-
+                    // 값을 비우고, maxAge를 0으로 주기
+                    ResponseCookie deleteCookie = generateCookie(name, "", 0, isSecure);
                     response.addHeader("Set-Cookie", deleteCookie.toString());
                     break;
                 }
