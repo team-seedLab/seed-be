59824ee5258f1ef4e6839fa44275e4b327eff683
yangjunsig <wnstlr1379@naver.com>
2026-03-15 12:02:46 +0900

    feature: 유저 정보 가져오는 api 생성

--- src/main/java/com/example/seedbe/domain/user/controller/UserController.java
+++ src/main/java/com/example/seedbe/domain/user/controller/UserController.java
@@ -1,11 +1,31 @@
 package com.example.seedbe.domain.user.controller;
 
+import com.example.seedbe.domain.user.dto.UserDetailResponse;
+import com.example.seedbe.domain.user.service.UserService;
 import com.example.seedbe.global.common.response.ApiResponse;
+import com.example.seedbe.global.security.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
+    public final UserService userService;
 
+    @Operation(
+            summary = "유저 정보 가져오기",
+            description = "유저 정보를 가져옵니다"
+    )
+    @GetMapping("/me")
+    public ApiResponse<UserDetailResponse> getUserDetails(
+            @AuthenticationPrincipal CustomUserDetails user
+    ) {
+        UserDetailResponse response = userService.getUserDetails(user.getUser().getUserId());
+        return ApiResponse.success(response);
+    }
 }
